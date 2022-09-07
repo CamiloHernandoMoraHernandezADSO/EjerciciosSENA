bf34ade41910928affe0891cfef6e422f8dc4966
Camilo Mora ADSO <chmora8@misena.edu.co>
2022-09-07 10:15:45 -0500

    Taller JavaScript

---
+++ JavaScript Miguel/Taller 1/main.js
@@ -0,0 +1,28 @@
+addEventListener("DOMContentLoader", ()=>{
+
+    //Ejercicio 1
+    //Escribir un programa que sume, reste, multiplique y divida dos números
+    
+    let suma, resta, multi,divi
+    let num1 = Number(prompt("Ingrese un numero"))
+    let num2 = Number(prompt("Ingrese un numero"))
+
+
+    suma = num1 + num2
+    resta = num1 - num2
+    multi = num1 * num2
+    divi= num1 / num2
+
+    alert(suma)
+    alert(resta)
+    alert(multi)
+    alert(divi)
+
+
+
+
+
+
+
+
+})
