af3ddd08760f4fc693316b6b8e013e151e8e259a
Camilo Mora ADSO <chmora8@misena.edu.co>
2022-09-07 11:23:42 -0500

    jercicio 16 JavaScript

--- JavaScript Miguel/Taller 1/main.js
+++ JavaScript Miguel/Taller 1/main.js
@@ -1,16 +1,27 @@
 addEventListener("DOMContentLoader", (e)=>{
 
 /*
-    Ejercicio 15
-    Hacer un diagrama para convertir de grados centígrados a grados Fahrenheit.
+    Ejercicio 16
+    Elaborar un algoritmo para calcular el promedio final de la materia de algoritmos. Dicha calificación se
+compone de los siguientes porcentajes:
+• 55% del promedio final de sus calificaciones de los tres(3) parciales.
+• 30% de la calificación examen final y
+• 15% de la calificación trabajo final.
 */
 
-    let gradosC, gradosF
+    let p1,p2,p3,exF,tF,promPar,promF
 
-    gradosC = Number(prompt("Ingrese los grados en centigrados"))
+    p1 = Number(prompt("Ingrese su nota parcial "))
+    p2 = Number(prompt("Ingrese su nota parcial "))
+    p3 = Number(prompt("Ingrese su nota parcial "))
+    exF = Number(prompt("Ingrese su nota del examen "))
+    tF = Number(prompt("Ingrese su nota Trabajo final "))
 
-    gradosF = (gradosC*(9/5))+32
+    promPar = (p1 + p2 + p3)/3
+    promF = promPar*0.55 + exF * 0.30 + tF * 0.15
+    
+    alert(`Nota final: ${promF}`)
 
-    alert(`Gradis Fahrenheit: ${gradosF}`)
+
 
 })
