93d6a5f16db2e178edfbf88af4f6aa1ccaf77085
Camilo Mora ADSO <chmora8@misena.edu.co>
2022-09-07 11:29:35 -0500

    Ejercicio 17 JavaScript

--- JavaScript Miguel/Taller 1/main.js
+++ JavaScript Miguel/Taller 1/main.js
@@ -1,27 +1,21 @@
 addEventListener("DOMContentLoader", (e)=>{
 
 /*
-    Ejercicio 16
-    Elaborar un algoritmo para calcular el promedio final de la materia de algoritmos. Dicha calificación se
-compone de los siguientes porcentajes:
-• 55% del promedio final de sus calificaciones de los tres(3) parciales.
-• 30% de la calificación examen final y
-• 15% de la calificación trabajo final.
+    Ejercicio 17
+Dado el valor que un cliente paga por un producto, calcular qué valor corresponde al costo total del
+producto y cuánto es el valor del IVA. Considerando que el porcentaje del IVA puede variar en el
+tiempo y de un producto a otro, este dato se lee por teclado.
+
 */
 
-    let p1,p2,p3,exF,tF,promPar,promF
+    let costo, iva,r
 
-    p1 = Number(prompt("Ingrese su nota parcial "))
-    p2 = Number(prompt("Ingrese su nota parcial "))
-    p3 = Number(prompt("Ingrese su nota parcial "))
-    exF = Number(prompt("Ingrese su nota del examen "))
-    tF = Number(prompt("Ingrese su nota Trabajo final "))
+    costo = Number(prompt("Ingrese el valor del producto"))
+    iva = Number(prompt("Ingrese el IVA"))
 
-    promPar = (p1 + p2 + p3)/3
-    promF = promPar*0.55 + exF * 0.30 + tF * 0.15
+    r = costo + iva/100
     
-    alert(`Nota final: ${promF}`)
-
+    alert(`Valor final: ${r}`)
 
 
 })
