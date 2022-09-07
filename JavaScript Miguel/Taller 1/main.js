3ce760b9416e0e60deabd536cb4e151af3be57ce
Camilo Mora ADSO <chmora8@misena.edu.co>
2022-09-07 10:54:56 -0500

    Ejercicio 9 JavaScript

--- JavaScript Miguel/Taller 1/main.js
+++ JavaScript Miguel/Taller 1/main.js
@@ -1,18 +1,20 @@
 addEventListener("DOMContentLoader", (e)=>{
 
-    //Ejercicio 8
-    //Escribir un programa que evalúe la siguiente expresión:
-    //(a+7*c)/(b+2-a)+2*b
-
-    let a,b,c,r
-
-    a = Number(prompt("ingrese un numero"))
-    b = Number(prompt("ingrese un numero"))
-    c = Number(prompt("ingrese un numero"))
-
-    r = (a+7*c)/(b+2-a)+2*b
-
-    alert(`Resultado ${r}`)
+    //Ejercicio 9
+    //Escribir un programa que calcule el área y el volumen de un cilindro:
+    //V = (PI * r2) * h
+    //A = (2 * (PI * r˄2)) + ((2 * PI * r) * h)
 
 
+    let v,a,h,r
+
+    r = Number(prompt("Ingrese el radio"))
+    h = Number(prompt("Ingrese la altura"))
+
+    v = Math.PI * Math.pow(r,2 * h)
+    a = (2 * (Math.PI * Math.pow(r,2)) + ((2 * Math.PI * r) * h))
+    
+
+    alert(`Volumen: ${v}`)
+    alert(`Area: ${a}`)
 })
