14b6611f0202893b66700ca1aa97d926df636fe6
Camilo Mora ADSO <chmora8@misena.edu.co>
2022-09-26 14:57:31 -0500

    Ejercicio 3 - Taller 2

--- JavaScript Miguel/Taller 2/main.js
+++ JavaScript Miguel/Taller 2/main.js
@@ -2,9 +2,9 @@ addEventListener("DOMContentLoaded", (e)=>{
 
 /* 
 
-Ejercicio 2
-    Calcule el mayor de tres números, permitiendo leer 3 valores diferentes
-
+Ejercicio 3
+    Desarrollar un diagrama que lea 3 valores diferentes e indique cual es el mayor de ellos, el menor o
+    si son iguales.
 */
 
     let num1, num2,num3
@@ -15,12 +15,21 @@ Ejercicio 2
 
     if(num1 > num2 && num1 > num3){
         alert(`${num1} es mayor que ${num2} y ${num3}`)
+    }else if(num1 < num2 && num1 < num3){
+        alert(`${num1} es menor que ${num2} y ${num3}`)
     }
-    else if(num2 > num1 && num2 > num3){
+    if(num2 > num1 && num2 > num3){
         alert(`${num2} es mayor que ${num1} y ${num3}`)
-    }    
-    else if(num3 > num2 && num3 > num1){
+    }else if(num2 < num1 && num2 < num3){
+        alert(`${num2} es menor que ${num1} y ${num3}`)
+    }
+    if(num3 > num2 && num3 > num1){
         alert(`${num3} es mayor que ${num1} y ${num2}`)
+    }else if(num3 < num2 && num3 < num1){
+        alert(`${num3} es menor que ${num1} y ${num2}`)
+    }
+    if(num1 == num2 || num1 == num3 || num2 == num3){
+        alert(`Hay números iguales`)
     }
 
 
